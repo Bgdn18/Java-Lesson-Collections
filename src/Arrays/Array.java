2e3622ce6a7a3bb2d892ed8ffdf0d4f4dd402889
Bgdn18 <krati18012012@gmail.com>
2025-05-24 19:38:41 +0400

    first commit

---
+++ src/Arrays/Array.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.Arrays; // Arrays
+
+public class Array {
+    public static void main(String[] args) {
+        short x = 18; // short "x" = 18
+        int y = 12; // int "y" = 12
+        byte z = 5; // byte "z" = 5
+
+        // First Method
+        int[] array1 = new int[3];
+        array1[0] = x; // Initialization array1 index: 0, number: x (18)
+        array1[1] = y; // Initialization array1 index: 1, number: y (12)
+        array1[2] = z; // Initialization array1 index: 2, number: z (5)
+
+        // Second Method
+        int[] array2 = {x,y,z}; // Initialization array2: x, y, z. Or 18, 12, 5
+
+        // Output
+        System.out.println("array1 without toString: " + array1); // Need a toString
+        System.out.println("array2 without toString: " + array2 + "\n"); // Need a toString
+
+        // Output with toString
+        System.out.println("array1 toString: " + Arrays.toString(array1));
+        System.out.println("array2 toString: " + Arrays.toString(array2) + "\n");
+
+        // To get "10" at index "1" you can write:
+        System.out.println("index 1 in array1 = " + array1[1]); // index "1" in "array1"
+        // and array2
+        System.out.println("index 1 in array2 = " + array2[1]); // index "1" in "array2"
+    }
+}
---
+++ src/ForEach/ForEach.java
@@ -0,0 +1,10 @@
+package ForEach;
+
+public class ForEach {
+    public static void main(String[] args) {
+        String[] numbers = {"1", "2", "3", "4"}; // Array "numbers"
+        for (String foreach : numbers) {
+            System.out.println(foreach);
+        }
+    }
+}
