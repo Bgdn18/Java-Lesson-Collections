5e801396dffcb240c9468d62d34bb5a8eb2d679f
Bgdn18 <krati18012012@gmail.com>
2025-06-02 02:08:05 +0400

    Cats and learning object-oriented programming

--- src/OOP/Cat/Cat.java
+++ src/OOP/Cat/Cat.java
@@ -6,6 +6,13 @@ public class Cat {
     String color;
     double weight;
 
+    // Humiliates its owner's couch.
+    public void destroySofa(String ownerSofa) // Waiting for the owner's name to be entered in parentheses
+    {
+        System.out.printf("Destroy sofa %s!", ownerSofa); // print
+    }
+
+    // Generate with alt + insert
     public Cat(String catName, String ownerName, String color, double weight) {
         this.catName = catName;
         this.ownerName = ownerName;
--- src/OOP/Cat/RunnerCat.java
+++ src/OOP/Cat/RunnerCat.java
@@ -6,5 +6,16 @@ public class RunnerCat {
         // Press Ctrl + P in IDEA if you forget which value to specify next
         Cat cat = new Cat("Vasya", "Sasha", "Red", 10.5); // Passing properties to cat
         System.out.println(cat);
+
+        System.out.println("____________________________________________");
+
+        System.out.println("Cat name: " + cat.catName);
+        System.out.println("Cat owner: " + cat.ownerName);
+        System.out.println("Cat color: " + cat.color);
+        System.out.println("Cat weight: " + cat.weight);
+
+        System.out.println("____________________________________________");
+
+        cat.destroySofa("Maksim");
     }
 }
