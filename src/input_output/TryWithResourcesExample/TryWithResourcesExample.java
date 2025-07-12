998e69d2138e2e74ad5aa894b340d4b03b64ee7c
bogdan <b.horolsky@gmail.com>
2025-07-13 00:43:22 +0400

    IO and try-with-resources

--- src/input_output/TryWithResourcesExample/TryWithResourcesExample.java
+++ src/input_output/TryWithResourcesExample/TryWithResourcesExample.java
@@ -1,16 +1,41 @@
 package input_output.TryWithResourcesExample;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class TryWithResourcesExample {
-    public static void main (String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String fileName = "textExample.txt"; // File name in resources folder
+
         // The stream is closed automatically
-        try (FileReader fileReader =
-                     new FileReader("Path_To_Text/textExample")) {
-            System.out.println("Successfully");
-        } catch (Exception e) {
-            System.out.println("Catching the error: " + e.getMessage());
+        try (InputStream inputStream = TryWithResourcesExample.class.getClassLoader()
+                .getResourceAsStream(fileName)) {
+
+            // Is there a file?
+            if (inputStream == null) {
+                System.err.printf("File '%s' not found", fileName);
+                return;
+            }
+
+            // Trying to read the file
+            try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8)) {
+                int lineCount = 0;
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    System.out.println(line);
+                    lineCount++;
+                }
+
+                System.out.println("Successfully output: " + lineCount + " lines");
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
         }
+        // All streams are closed
     }
 }
--- src/input_output/reading_files/FileInputExample.java
+++ src/input_output/reading_files/FileInputExample.java
@@ -4,22 +4,22 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
-public class FIleInputExample {
+public class FileInputExample {
     /* Check resources folder */
 
     public static void main(String[] args) {
-        InputStream inputStream = FIleInputExample.class.getClassLoader()
-                .getResourceAsStream("textExample");
+        InputStream inputStream = FileInputExample.class.getClassLoader()
+                .getResourceAsStream("textExample.txt");
 
         Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8);
         int count = 0;
-        int lineNeeded = 9;
+        //int lineNeeded = 1;
 
         while (scanner.hasNext()) {
             String nextLine = scanner.nextLine();
-            if (count == lineNeeded) {
+            //if (count == lineNeeded) {
                 System.out.println(nextLine);
-            }
+            //}
             count++;
         }
     }
